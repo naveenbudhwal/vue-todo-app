aa4ecc235e35b61b2b6ef40521c2760a9e0a5ff1
naveenbudhwal <naveenbudhwal@gmail.com>
2019-07-21 13:18:19 +0530

    Connecting to mongo database

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,24 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const mongo = require('mongodb')
+
+const MongoClient = mongo.MongoClient
+const uri = "mongodb+srv://default-user:aadjusting@cluster0-peiej.mongodb.net/test?retryWrites=true&w=majority"
+var client
+
+var mongoClient = new MongoClient(uri, {
+  reconnectTries:
+    Number.MAX_VALUE, autoReconnect: true, useNewUrlParser: true
+})
+
+mongoClient.connect((err, db) => { // returns db connection
+  if (err != null) {
+    console.log(err)
+    return
+  }
+  client = db
+})
 
 const app = express()
 
@@ -10,10 +28,16 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/todo', (req, res) => {
-  res.send([
-    'Thing 1',
-    'Thing 2'
-  ])
+  const collection = client.db("test").collection("todos")
+  collection.find().toArray(function (err, results) {
+    if (err) {
+      console.log(err)
+      res.send([])
+      return
+    }
+
+    res.send(results)
+  })
 })
 
 app.listen(process.env.PORT || 8082)
